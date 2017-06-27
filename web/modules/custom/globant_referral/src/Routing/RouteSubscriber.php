3f874ec2e1e636d6636c7f4f2a69fed8d57ffbdd
Rakesh Falke <rakesh.falke@globant.com>
2017-06-27 16:42:11 +0000

    More permissions

--- web/modules/custom/globant_referral/src/Routing/RouteSubscriber.php
+++ web/modules/custom/globant_referral/src/Routing/RouteSubscriber.php
@@ -15,19 +15,19 @@ class RouteSubscriber extends RouteSubscriberBase {
   protected function alterRoutes(RouteCollection $collection) {
     // Always deny access to '/user/register'.
     if ($route = $collection->get('user.register')) {
-      $route->setRequirement('_permission', 'administer globantuser');
+      $route->setRequirement('_permission', 'administer globantedituser');
     }
     // Always deny access to '/user/password'.
     if ($route = $collection->get('user.pass')) {
-      $route->setRequirement('_permission', 'administer globantuser');
+      $route->setRequirement('_permission', 'administer globantedituser');
     }
     // Always deny access to '/user/{user}/edit'.
     if ($route = $collection->get('entity.user.edit_form')) {
-      $route->setRequirement('_permission', 'administer globantuser');
+      $route->setRequirement('_permission', 'administer globantedituser');
     }
     // Always deny access to '/user/{user}'.
     if ($route = $collection->get('user.page')) {
-      $route->setRequirement('_permission', 'administer globantuser');
+      $route->setRequirement('_permission', 'administer globantedituser');
     }
   }
 }
